1ce7be2d8297d6725fe878be98697ac67d8baf91
Icetrixwastaken <140248087+Icetrixwastaken@users.noreply.github.com>
2025-10-03 15:44:07 +0800

    df
    df

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    CLIENT_ID: "0000000000000000000", // Discord Application ID
-    COMPANY_NAME: "TheSleepyKoala", // Company or Developer Name
-    PRODUCT_NAME: "Pong with GDScript", // Game Name
+    CLIENT_ID: "1423225142892564511", // Discord Application ID
+    COMPANY_NAME: "Icetrix", // Company or Developer Name
+    PRODUCT_NAME: "Chameleon", // Game Name
     PRODUCT_VERSION: "0.1.0", // Game Version
 };
